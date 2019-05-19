5c3adc4470a2f9ef2e395360d81baf04ac164665
Mark van den Broek <mvdnbrk@gmail.com>
2019-05-19 13:47:09 +0200

    Update message.

--- index.js
+++ index.js
@@ -2,11 +2,7 @@ let mix = require('laravel-mix');
 
 class Tailwind {
     dependencies() {
-        this.requiresReload = `
-            Tailwind has now been installed. Please ensure that
-            your tailwind.js configuration file (node_modules/.bin/tailwind init)
-            has been created, and then run "npm run dev" again.
-        `;
+        this.requiresReload = `Tailwind has now been installed. Please run "npm run dev" again.`;
 
         return ['tailwindcss'];
     }
