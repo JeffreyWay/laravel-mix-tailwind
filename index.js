f0dc0c88a577803233d7adee68b7aedab1bac042
Ebben Feagan <ebben.feagan@gmail.com>
2019-08-02 19:16:21 -0500

    Fix default configuration filename
    The default Tailwind configuration filename is tailwind.config.js

--- index.js
+++ index.js
@@ -4,14 +4,14 @@ class Tailwind {
     dependencies() {
         this.requiresReload = `
             Tailwind has now been installed. Please ensure that
-            your tailwind.js configuration file (node_modules/.bin/tailwind init)
+            your tailwind.config.js configuration file (node_modules/.bin/tailwind init)
             has been created, and then run "npm run dev" again.
         `;
 
         return ['tailwindcss'];
     }
 
-    register(configPath = './tailwind.js') {
+    register(configPath = './tailwind.config.js') {
         this.configPath = configPath;
     }
 
